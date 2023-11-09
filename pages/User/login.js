2067021d01b9ae674ab36b6776a5c7e12e639fd2
ErickBezerrar <erickbrtrindade@gmail.com>
2023-11-09 15:13:34 -0300

    Index and Login page conection

--- pages/User/login.js
+++ pages/User/login.js
@@ -3,7 +3,8 @@ import Link from 'next/link'
 export default function Login(){
     return(
     <div>
-        Página de Login, se vira, Manuelly!
+        <Link href="/">Home</Link>
+        <p>Página de Login, se vira, Manuelly!</p>
     </div>
     )
 }
