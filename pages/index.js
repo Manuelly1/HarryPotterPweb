18a04ef9e97f7ae0c92527791c6a7365b5d78ef3
ErickBezerrar <erickbrtrindade@gmail.com>
2023-11-08 21:21:27 -0300

    First main page HTML

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,12 @@ import Link from 'next/link'
 export default function Main(){
     return(
     <div>
-        <img src="https://img.elo7.com.br/product/original/1C6878E/painel-2x1-harry-potter-magicos.jpg"></img>
+        <div>
+            <Link href="/pages/login.js">
+                <img src="HarryPotterPweb\images\kisspng-computer-icons-user-profile-encapsulated-postscrip-icon-black-5b45d010037271.6274823915313019040141.jpg"></img>
+            </Link>
+            <img src="https://img.elo7.com.br/product/original/1C6878E/painel-2x1-harry-potter-magicos.jpg"></img>
+        </div>
         <div className="all-movies">
             <div className="movie" src="aimagemficaráaqui"></div>
         </div>
