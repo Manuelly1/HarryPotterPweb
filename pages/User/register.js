49a8b25b8580bde84ec7a0bbd18f535c68e1da20
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-11-11 12:05:50 -0300

    update na estilização

--- pages/User/register.js
+++ pages/User/register.js
@@ -9,10 +9,10 @@ export default function Register() {
     const [selectedHouse, setSelectedHouse] = useState(null);
 
     const houses = [
-        { name: 'Gryffindor', url: 'https://i.pinimg.com/564x/7e/8c/2e/7e8c2e995e1d8a41fa7d13034edb24ad.jpg'},
-        { name: 'Hufflepuff', url: 'https://i.pinimg.com/564x/76/45/b9/7645b9b88e14bc3d8c12954bb130fd76.jpg'},
-        { name: 'Ravenclaw', url: 'https://i.pinimg.com/564x/6d/ed/03/6ded03a78ba6b8d870c899586117245a.jpg'},
-        { name: 'Slytherin', url: 'https://i.pinimg.com/564x/e7/6c/57/e76c57c8c4352a05c3c573fe1fba08d8.jpg'},
+        {name: 'Gryffindor', url: 'https://i.pinimg.com/564x/7e/8c/2e/7e8c2e995e1d8a41fa7d13034edb24ad.jpg'},
+        {name: 'Hufflepuff', url: 'https://i.pinimg.com/564x/76/45/b9/7645b9b88e14bc3d8c12954bb130fd76.jpg'},
+        {name: 'Ravenclaw', url: 'https://i.pinimg.com/564x/6d/ed/03/6ded03a78ba6b8d870c899586117245a.jpg'},
+        {name: 'Slytherin', url: 'https://i.pinimg.com/564x/e7/6c/57/e76c57c8c4352a05c3c573fe1fba08d8.jpg'},
     ];
 
     return (
@@ -22,7 +22,7 @@ export default function Register() {
                     <Input type="email" placeholder="Informe um e-mail:"/> 
                     <Input type="login" placeholder="Informe um login:"/> 
                     <Input type="password" placeholder="Informe uma senha:"/>
-                    <p className={styles.titleQuestion}>Selecione a casa de Hogwarts que você pertence:</p>
+                    <p className={styles.titleQuestion}>Selecione a sua casa em Hogwarts:</p>
                     <div className={styles.houseSelection}>
                         {houses.map((house, index) => (
                             <div key={index} className={styles.houseOption}>
