e1fd4eeea3bf60ce3c1698b1fe0f9ed846024175
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-11-16 21:02:52 -0300

    nova tela (80%)

--- pages/Movies/moviesDetails.js
+++ pages/Movies/moviesDetails.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import styles from '../styles/MoviesDetails.module.css';
 import IconBack from '../components/icons/iconBack';
 import DetailsCard from '../components/detailsCard/detailsCard';
-import DetailsCardTitle from '../components/detailsCard/detailsTitle';
 
 export default function MoviesDetails({  }) {
   return (
@@ -10,18 +9,24 @@ export default function MoviesDetails({  }) {
         <Link className={styles.linkHome} href="/">
             <IconBack />
         </Link>
-        <DetailsCardTitle title="Detalhes" className={styles.specialCard}/>
         <div className={styles.cardsContainer}>
             <DetailsCard
                 image="https://w.forfun.com/fetch/cd/cdb10fb1fa03fd60e3f73fcce3fcad6c.jpeg"
                 title = "Harry Potter e as Relíquias da Morte: Parte 2"
+                year = "2011"
                 time = "130 minutos"
                 type = "Fantasia"
                 sinopse = "Sinopse"
                 description= "A batalha entre as forças do bem e do mal da magia alcançam o mundo dos trouxas. O risco nunca foi tão grande, e ninguém está seguro. Harry Potter precisa fazer um sacrifício final conforme o confronto com Lord Voldemort se aproxima."
             />
+            <Link href="characters">
+                <button className={styles.buttonPersonagens}>Personagens</button>        
+            </Link>
+            <button className={styles.buttonCheck}>Já vi este filme</button>
         </div>
     </div>
   );
 }
+
+// só falta colocar um link para ver o filme (colocar no formato do "já possui uma conta?")
 
--- pages/components/detailsCard/detailsCard.js
+++ pages/components/detailsCard/detailsCard.js
@@ -1,18 +1,31 @@
 import styles from './detailsCard.module.css';
+import IconTime from '../../components/icons/IconTime';
+import IconCategory from '../../components/icons/iconCategory';
+import IconCalendar from '../../components/icons/iconCalendar';
 
-export default function DetailsCard({ title, image, time, type, description, sinopse }) {
+export default function DetailsCard({ title, year, image, time, type, description, sinopse }) {
     return (
       <div className={styles.card}>
-        <h2 className={styles.title}>{title}</h2>
+        <h1 className={styles.title}>{title}</h1>
         <div className={styles.imageContainer}>
           <img className={styles.image} src={image} alt={`Imagem de ${title}`} />
         </div>
         <div className={styles.movieInfo}>
-          <p className={styles.time}>{time}</p>
-          <p className={styles.type}>{type}</p>
-          <h3 className={styles.sinopse}>{sinopse}</h3>
-          <p className={styles.description}>{description}</p>
+            <div className={styles.iconCalendar}>
+                <IconCalendar/>
+                <p className={styles.year}>{year}</p>
+            </div>
+            <div className={styles.iconTime}>
+                <IconTime />
+                <p className={styles.time}>{time}</p>
+            </div>
+            <div className={styles.iconCategory}>
+                <IconCategory/>
+                <p className={styles.type}>{type}</p>
+            </div>
+            <h3 className={styles.sinopse}>{sinopse}</h3>
+            <p className={styles.description}>{description}</p>
         </div>
-      </div>
+        </div>
     );
-  }
+}
