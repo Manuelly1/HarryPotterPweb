13f2fd3d8f1da9465cc9d938953dc09bd79ee989
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-11-22 21:56:01 -0300

    atualizando da estilização de algumas pág

--- pages/components/housesCard/housesCardTitle.js
+++ pages/components/housesCard/housesCardTitle.js
@@ -3,7 +3,7 @@ import styles from './housesCard.module.css'
 export default function HousesCardTitle({ title, children }) { 
     return (
         <div className={styles.card}>
-            <h2 className={styles.title}>{title}</h2>
+            <h2 className={styles.titleOne}>{title}</h2>
             {children}
         </div>
     )
