9345c65a838103003e87b476aed5903d84e9758d
DayXL <day.luc.xav0606@gmail.com>
2023-12-02 18:11:57 -0300

    criado modelo de cards de personagens

--- pages/Movies/characters.js
+++ pages/Movies/characters.js
@@ -4,46 +4,47 @@ import styles from '../styles/Characters.module.css';
 import IconHome from '../components/icons/iconHome';
 import IconSearch from '../components/icons/iconSearch';
 import IconBack from '../components/icons/iconBack';
+import CharactersCard from '../components/characters/charactersCard';
 
 const charactersData = [
     {
         id: 1,
-        name: "Harry Potter",
+        nome: "Harry Potter",
         image: "https://imagem.band.com.br/f_480959.jpg",
     },
     {
         id: 2,
-        name: "Draco Malfoy",
+        nome: "Draco Malfoy",
         image: "https://i.pinimg.com/474x/32/c3/7e/32c37e565a7138117dbe2dbbcc61b158.jpg",
     },
     {
         id: 3,
-        name: "Hermione Granger",
+        nome: "Hermione Granger",
         image: "https://images7.alphacoders.com/726/726078.jpg",
     },
     {
         id: 4,
-        name: "Ron Weasley",
+        nome: "Ron Weasley",
         image: "https://wallpapercave.com/wp/wp1965058.jpg",
     },
     {
         id: 5,
-        name: "Lord Voldemort",
+        nome: "Lord Voldemort",
         image: "https://images7.alphacoders.com/556/556455.jpg",
     },
     {
         id: 6,
-        name: "Severus Snape",
+        nome: "Severus Snape",
         image: "https://c4.wallpaperflare.com/wallpaper/437/200/42/snape-harry-potter-alan-wallpaper-preview.jpg",
     },
     {
         id: 7,
-        name: "Albus Dumbledore",
+        nome: "Albus Dumbledore",
         image: "https://i.pinimg.com/originals/b7/be/1e/b7be1e2cb786303c378406895a591f02.jpg",
     },
     {
         id: 8,
-        name: "Dobby",
+        nome: "Dobby",
         image: "https://rollingstone.uol.com.br/media/uploads/dobby_em_harry_potter_e_camara_secreta_foto_reproducao__imdb.jpg",
     },
 ];
@@ -56,12 +57,19 @@ export default function Characters() {
                 {
                     charactersData.map((character) => (
                         <div key={character.id} className={styles.characterCard}>
-                        <img src={character.image} alt={character.name} className={styles.characterImageName}/>
-                        <h2>{character.name}</h2>
+                            <CharactersCard 
+                                nome={character.nome} 
+                                image={character.image} 
+                                ator = {"character.ator"}
+                                descricao = {"descricao bla bla bla"}
+                                filmes = {["filme1", "filme2", "filme3"]}
+                                casa = {Math.floor(character.id / 3)}
+                            />
                         </div>
                         )
                     )
                 }
+                
                 </div>
                 <div className={styles.searchIcon}>
                     <IconSearch />
---
+++ pages/components/characters/charactersCard.js
@@ -0,0 +1,27 @@
+import styles from './charactersCard.module.css';
+
+const colorsMapping = [
+    '#222F5B', // Corvinal
+    '#740001', // Grifinória
+    '#1A472A', // Sonserina
+    '#FFD700', // Lufa-Lufa
+    '#333333', // Sem Casa
+];
+  
+export default function CharactersCard({ nome, image, ator, descricao, filmes, casa }) {
+    const corFundo = casa ? colorsMapping[casa] : colorsMapping[4]; // 4 é o índice da cor para 'Sem Casa'
+
+    return (
+        <div className={styles.card} style={{ backgroundColor: corFundo }}>
+            <h2 className={styles.nome}>{nome}</h2>
+            <div className={styles.imageContainer}>
+                <img className={styles.image} src={image} alt={nome} />
+            </div>
+            <div className={styles.houseInfo}>
+                <p className={styles.description}>{descricao}</p>
+                <p className={styles.actor}>Ator/Atriz: {ator}</p>
+                <p className={styles.movies}>Filmes: {filmes.join(', ')}</p>
+            </div>
+        </div>
+    );
+}
