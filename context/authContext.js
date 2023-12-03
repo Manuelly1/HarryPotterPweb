47d353074f1eb2f887683ecbccece645db69a294
DayXL <day.luc.xav0606@gmail.com>
2023-12-03 12:17:24 -0300

    criado arquivo para controle de logado

---
+++ context/authContext.js
@@ -0,0 +1,24 @@
+// authContext.js
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logIn = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logOut = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
