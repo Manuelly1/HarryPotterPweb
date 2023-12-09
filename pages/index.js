f886f0713eb746895534e9e178f310ead6d78d13
DayXL <day.luc.xav0606@gmail.com>
2023-12-09 09:47:12 -0300

    ajeitado janela de confirmar logOut

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import IconUser from './components/icons/iconUser';
 import IconHouses from './components/icons/iconHouses';
 import IconHome from './components/icons/iconHome';
 import IconExit from './components/icons/iconExit';
-import { open_close_modal } from './components/exitCard/exitCard';
 import { getMoviesData } from '../api/moviesApi';
 import { useAuth } from '../context/authContext';
 
@@ -23,18 +22,22 @@ const testeimagensmainpage = [
 export default function Main({ moviesData }) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
-    const { isLoggedIn, userDetails, logout } = useAuth();
+    const { isLoggedIn, logOut } = useAuth();
+
+    const openClodeModal = () => {
+      setShowLogoutConfirmation(!showLogoutConfirmation);
+    };
   
     const handleLogoutClick = () => {
         if (isLoggedIn) {
-            open_close_modal(true);
+            openClodeModal();
         } else {
           window.alert('Você precisa estar logado para realizar o logout');
         }
       };
   
     const handleLogoutConfirmed = () => {
-      logout();
+      logOut();
       setShowLogoutConfirmation(false);
     };
   
