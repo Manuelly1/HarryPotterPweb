800f00e767af631d244d8f96a39c3d3fd01cb7b0
DayXL <day.luc.xav0606@gmail.com>
2023-12-15 08:55:42 -0300

    precisa logar para comentar

--- pages/Movies/comment.js
+++ pages/Movies/comment.js
@@ -3,21 +3,29 @@ import CommentCard from '../components/commentCard/commentCard';
 import styles from '../styles/Comment.module.css';
 import IconBin from '../components/icons/iconBin';
 import IconChange from '../components/icons/iconChange';
+import { useAuth } from '../../context/authContext';
 
 export default function Comment() {
     const [userComment, setUserComment] = useState("");
     const [comments, setComments] = useState([]);
+    const [showNotLoggedIn, setShowNotLoggedIn] = useState(false);
+    const { isLoggedIn} = useAuth();
 
     const handleCommentChange = (event) => {
         setUserComment(event.target.value);
     };
 
     const handleCommentSubmit = (event) => {
-        event.preventDefault();
-        console.log("Comentário do usuário:", userComment);
+        if (isLoggedIn) {
+            event.preventDefault();
+            console.log("Comentário do usuário:", userComment);
 
-        setComments([...comments, { text: userComment, id: Date.now() }]);
-        setUserComment("");
+            setComments([...comments, { text: userComment, id: Date.now() }]);
+            setUserComment("");
+        }
+        else {
+            setShowNotLoggedIn(true);
+        }
     };
 
     const handleCommentEdit = (id) => {
@@ -28,6 +36,10 @@ export default function Comment() {
     const handleCommentDelete = (id) => {
         // Implement delete functionality as needed
         setComments(comments.filter(comment => comment.id !== id));
+    };
+
+    const handleLNotLoggedIn = () => {
+        setShowNotLoggedIn(false);
     };
 
     return (
@@ -66,6 +78,13 @@ export default function Comment() {
             <button type="submit" className={styles.button} onClick={handleCommentSubmit}>
                 Compartilhar comentário
             </button>
+            
+            {showNotLoggedIn && (
+                <div className={styles.notLoggedIn}>
+                    <p>Precisa logar para reagir</p>
+                    <button onClick={handleLNotLoggedIn}>Ok</button>
+                </div>
+            )}
 
             <style jsx global>
                 {`
